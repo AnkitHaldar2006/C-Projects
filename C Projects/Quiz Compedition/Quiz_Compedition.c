27cfec959a1c7af86b6b412fc63d1107c87ca2b4
AnkitHaldar2006 <haldarankit2006@gmail.com>
2025-07-17 14:35:56 +0530

    Quiz_Compedition.c

--- C Projects/Quiz Compedition/Quiz_Compedition.c
+++ C Projects/Quiz Compedition/Quiz_Compedition.c
@@ -1,107 +1,142 @@
-#include<stdio.h>
-int main(){
-	char answer;
-	int score=0;
-	printf("Welcome to the Quiz Game\n");
-	printf("Enter your Answer(A/B/C/D) For Each Question.\n");
-	
-//Question 1	
-	printf("1.What is the Capital Of our India?\n");
-	printf("A. Delhi\nB. Mumbai\nC. Kolkata\nD. Chennai\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='A'||answer=='a')score++;
+#include <stdio.h>
 
-	
-//Question 2	
-	printf("2.Who is the founder of Microsoft?\n");
-	printf("A. Elon Musk\nB. Bill Gates\nC. Steve Jobs\nD. Mark Zuckerberg\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='B'||answer=='b')score++;
+int main() {
+    char answer;
+    int score = 0;
 
-	
-//Question 3	
-	printf("3.What is the value of 5*5+2?\n");
-	printf("A. 20\nB. 30\nC. 27\nD. 25\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='C'||answer=='c')score++;
+    printf(" Welcome to the Quiz Game\n");
+    printf("Enter your Answer (A/B/C/D) For Each Question.\n");
 
-	
-//Question 4	
-	printf("4.Who is the CEO of Google?\n");
-	printf("A. Sundar Bose\nB. Sundar Roy\nC. Sudar lal\nD. Sundar Pichai\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='D'||answer=='d')score++;
+    // Question 1
+    printf("1. What is the Capital Of India?\n");
+    printf("A. Delhi\nB. Mumbai\nC. Kolkata\nD. Chennai\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'A' || answer == 'a') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is A. Delhi\n");
+    }
 
-	
-//Question 5	
-	printf("5.Which is the largest ocean?\n");
-	printf("A. Pacific\nB. Indian\nC. Arctic\nD. Atlantic\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='A'||answer=='a')score++;
+    // Question 2
+    printf("2. Who is the founder of Microsoft?\n");
+    printf("A. Elon Musk\nB. Bill Gates\nC. Steve Jobs\nD. Mark Zuckerberg\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'B' || answer == 'b') {
+        score++;
+        printf(" Correct!\n\n");
+    } else {
+        printf(" Wrong! The correct answer is B. Bill Gates\n");
+    }
 
-	
-//Question 6	
-	printf("6.Which planet is known as the Red Planet??\n");
-	printf("A. Earth\nB. Mars\nC. Jupiter\nD. Pluto\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='B'||answer=='b')score++;
+    // Question 3
+    printf("3. What is the value of 5*5+2?\n");
+    printf("A. 20\nB. 30\nC. 27\nD. 25\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'C' || answer == 'c') {
+        score++;
+        printf(" Correct!\n\n");
+    } else {
+        printf(" Wrong! The correct answer is C. 27\n");
+    }
 
-	
-//Question 7	
-	printf("7.Which is the latest model of Samsung S Series?\n");
-	printf("A. Galaxy S23\nB. Galaxy S25+\nC. Galaxy S25\nD. Galaxy S21\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='C'||answer=='c')score++;
+    // Question 4
+    printf("4. Who is the CEO of Google?\n");
+    printf("A. Sundar Bose\nB. Sundar Roy\nC. Sudar Lal\nD. Sundar Pichai\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'D' || answer == 'd') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is D. Sundar Pichai\n");
+    }
 
-	
-//Question 8	
-	printf("8.Which Country invented UPI payment?\n");
-	printf("A. Austrelia\nB. Brazil\nC. Canada\nD. India\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='D'||answer=='d')score++;
+    // Question 5
+    printf("5. Which is the largest ocean?\n");
+    printf("A. Pacific\nB. Indian\nC. Arctic\nD. Atlantic\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'A' || answer == 'a') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is A. Pacific\n");
+    }
 
-	
-//Question 9	
-	printf("9.How many times Brazil Won Fifa World Cup?\n");
-	printf("A. 5\nB. 3\nC. 6\nD. 4\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='A'||answer=='a')score++;
+    // Question 6
+    printf("6. Which planet is known as the Red Planet?\n");
+    printf("A. Earth\nB. Mars\nC. Jupiter\nD. Pluto\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'B' || answer == 'b') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is B. Mars\n");
+    }
 
-	
-//Question 10	
-	printf("10.Which year Mohonbagan won the first Shield?\n");
-	printf("A. on July 22, 1912\nB. on June 29, 1911\nC. on October 10, 1908\nD. on January 12, 1910\n");
-    printf("your Answer: ");
-    scanf(" %c",&answer);
-if(answer=='B'||answer=='b')score++;
+    // Question 7
+    printf("7. Which is the latest model of Samsung S Series?\n");
+    printf("A. Galaxy S23\nB. Galaxy S25+\nC. Galaxy S25\nD. Galaxy S21\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'C' || answer == 'c') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is C. Galaxy S25\n");
+    }
 
+    // Question 8
+    printf("8. Which country invented UPI payment?\n");
+    printf("A. Australia\nB. Brazil\nC. Canada\nD. India\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'D' || answer == 'd') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is D. India\n");
+    }
 
-//Result
-printf("Your Scored %d Out of 10!\n");
+    // Question 9
+    printf("9. How many times has Brazil won the FIFA World Cup?\n");
+    printf("A. 5\nB. 3\nC. 6\nD. 4\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'A' || answer == 'a') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is A. 5\n");
+    }
 
-if(score==10){
-	
-printf("Excellent\n");
-	
-}else if(score>=5){
+    // Question 10
+    printf("10. Which year did Mohun Bagan win the first Shield?\n");
+    printf("A. July 22, 1912\nB. June 29, 1911\nC. October 10, 1908\nD. January 12, 1910\n");
+    printf("Your Answer: ");
+    scanf(" %c", &answer);
+    if (answer == 'B' || answer == 'b') {
+        score++;
+        printf(" Correct!\n");
+    } else {
+        printf(" Wrong! The correct answer is B. June 29, 1911\n");
+    }
 
+    // Final Result
+    printf(" You scored %d out of 10!\n", score);
 
-printf("Good Job!\n");
+    if (score == 10) {
+        printf(" Excellent!\n");
+    } else if (score >= 5) {
+        printf(" Good Job!\n");
+    } else {
+        printf(" Better luck next time.\n");
+    }
 
-}else{
-	
-	printf("Better luck Next Time.");
-	
-}
-
-return 0;
+    return 0;
 }
