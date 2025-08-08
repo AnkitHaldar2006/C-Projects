61d9e0dcdc39fe38c6ee890b04caada2cb8e1db9
AnkitHaldar2006 <haldarankit2006@gmail.com>
2025-08-08 14:22:13 +0530

    Add files via upload

---
+++ C Projects/2D Matrix Of Array/2D_Matrix_of_an_Array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[2][3]; // 2 rows, 3 columns
+    int i, j;
+
+    printf("Enter 6 elements (2x3 matrix):\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
