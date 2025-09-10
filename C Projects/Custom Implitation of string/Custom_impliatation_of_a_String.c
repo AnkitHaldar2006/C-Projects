55a78a7cdf24813f0ce1b5602a86a8e6ffc6c016
AnkitHaldar2006 <haldarankit2006@gmail.com>
2025-09-10 21:50:18 +0530

    Add files via upload

---
+++ C Projects/Custom Implitation of string/Custom_impliatation_of_a_String.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int stringLength(char str[]) {
+    int length = 0;
+    while (str[length] != '\0')
+        length++;
+    return length;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    gets(str);
+
+    printf("Length of string: %d\n", stringLength(str));
+    return 0;
+}
